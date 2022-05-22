40bcb8fea70f470c25061d46d77add8b9a0373d7
Wynton Franklin <wynton.franklin@gmail.com>
2022-05-22 16:49:09 -0400

    add arg helper

--- index.js
+++ index.js
@@ -1,10 +1,29 @@
 const path = require("path");
 const fs = require("fs");
+const yargs = require('yargs/yargs')
+const { hideBin } = require('yargs/helpers')
 const flowEngine = require("./assets/engine");
+
+const args = yargs(hideBin(process.argv))
+    .option('file', {
+        alias: 'f',
+        type: 'string',
+        description: 'File to run'
+    })
+    .option('verbose', {
+        alias: 'v',
+        type: 'boolean',
+        description: 'Run with verbose logging'
+    })
+    .parse()
+
+
+
 
 let output = "";
 //let filename = "C:\\Users\\wfranklin\\Desktop\\wftutorials\\New folder\\char count.flowpro";
-let filename = "C:\\Users\\wfranklin\\Desktop\\wftutorials\\test.flowpro";
+
+let filename = args.file;
 
 
 if(filename !== null && filename !== undefined) {
